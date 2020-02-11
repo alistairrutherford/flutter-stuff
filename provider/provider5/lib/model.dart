7bd7e9072a0c00294256bd9333cf21431fd7c442
alistairtutherford <alistair.rutherford@mottmac.com>
2020-02-11 16:16:07 +0000

    Update data classes.

--- provider/provider5/lib/model.dart
+++ provider/provider5/lib/model.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/foundation.dart';
-import 'package:provider5/Headlines.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart';
+import 'package:provider5/news.dart';
 
 class NewsModel extends ChangeNotifier {
   static const String HEADLINES_DATA = "assets/data/headlines.json";
 
-  Headlines _headlines;
+  News _news;
 
-  Headlines get headlines => _headlines;
-  set(Headlines headlines) => _headlines = headlines;
+  News  get headlines => _news;
+  set(News news) => _news = news;
 
   Future updateHeadlines() async {
     // Fetch json
     var dataString = await loadAsset(HEADLINES_DATA);
 
-    _headlines = loadHeadlines(dataString);
+    _news = loadNews(dataString);
 
     notifyListeners();
   }
 
-  Headlines loadHeadlines(String dataString) {
+  News loadNews(String dataString) {
     Map<String, dynamic> jsonUserData = jsonDecode(dataString);
-    Headlines headlines = Headlines.map(jsonUserData);
+    News headlines = News.fromMap(jsonUserData);
     return headlines;
   }
 
--- provider/provider5/test/widget_test.dart
+++ provider/provider5/test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:provider5/Headlines.dart';
 import 'package:provider5/model.dart';
+import 'package:provider5/news.dart';
 
 const String USERS = '{"status":"ok","totalResults":38,"articles":[{"source":{"id":null,"name":"Elevenwarriors.com"},"author":null,"title":"Mark Dantonio Stepping Down As Michigan States Head Coach After 13 Seasons | Eleven Warriors - Eleven Warriors","description":"Mark Dantonio announced Tuesday that he will step down as Michigan States head coach following 13 seasons with the Spartans.","url":"https://www.elevenwarriors.com/the-big-ten/2020/02/111987/mark-dantonio-stepping-down-as-michigan-states-head-coach-after-13-seasons","urlToImage":"https://www.elevenwarriors.com/sites/default/files/styles/904x490/public/c/2020/02/111987_h.jpg?itok=pYrkcaQk","publishedAt":"2020-02-04T19:54:31Z","content":"Mark Dantonios tenure as Michigan States head coach has come to an end. \r\nAfter 13 years leading Michigan States football program, Dantonio announced Tuesday that he has decided to step down as the Spartans head coach.\r\nTo all Spartans: Thank you for ever"}]}';
 
@@ -17,9 +17,9 @@ void main() {
   testWidgets('Test load', (WidgetTester tester) async {
     NewsModel newsModel = NewsModel();
 
-    Headlines headlines = newsModel.loadHeadlines(escape(USERS));
+    News news  = newsModel.loadNews(escape(USERS));
 
-    // Verify that our counter has incremented.
-    expect(headlines, isNotEmpty);
+    // Verify data has been read.
+    expect(news, isNotNull);
   });
 }
