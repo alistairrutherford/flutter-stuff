7d9cd8bb1e71cad38e182a1c4d85b55e83be85f3
alistairrutherford <alistair.rutherford@gmail.com>
2023-11-01 22:58:06 +0000

    Add initial journey map view.

---
+++ flutter_db_provider/lib/journey_map_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class JourneyMapPage extends StatelessWidget {
+  const JourneyMapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
