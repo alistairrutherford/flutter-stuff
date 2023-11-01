ea4e14021a1bb42a13161b13e1c6d9730b94efb2
alistairrutherford <alistair.rutherford@gmail.com>
2023-11-01 21:03:35 +0000

    Re-factored map page to become StatefulWidget and maintain map ready state so we know when to draw position marker.

--- flutter_db_provider/lib/map_page.dart
+++ flutter_db_provider/lib/map_page.dart
@@ -6,40 +6,61 @@ import 'package:url_launcher/url_launcher.dart';
 
 import 'location_model.dart';
 
-class MapPage extends StatelessWidget {
-  final MapController _mapController = MapController();
-
-  MapPage({super.key});
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    const double defaultZoom = 17;
+  State<MapPage> createState() => _MapPageState();
+}
 
-    var locationModel = context.watch<LocationModel>();
+class _MapPageState extends State<MapPage> {
+  static const double defaultZoom = 17;
+  final MapController _mapController = MapController();
+  Marker? _marker;
+  LatLng? _latLng;
+  bool _mapReady = false;
 
+  void updateView(LocationModel locationModel) {
     // Determine current location (if any).
-    LatLng latLng =  (locationModel.currentPosition != null)
+    _latLng = (locationModel.currentPosition != null)
         ? (LatLng(locationModel.currentPosition!.latitude,
         locationModel.currentPosition!.longitude))
         : (const LatLng(51.5072, 0.1276));
 
+    _marker = Marker(
+      width: 10,
+      height: 10,
+      point: _latLng!,
+      child: const Icon(Icons.location_history_rounded ),
+      alignment: const Alignment(0,-2.5)
+    );
+
+    if (_mapReady) {
+      _mapController.move(_latLng!, defaultZoom);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var locationModel = context.watch<LocationModel>();
+
+    // Every time the model notifies a change the view will update.
+    updateView(locationModel);
+
     return FlutterMap(
       mapController: _mapController,
       options: MapOptions(
-        initialCenter: latLng,
         initialZoom: defaultZoom,
-        onMapEvent: (e) {
-          _mapController.move(latLng, defaultZoom);
-        },
         onMapReady: () {
-          _mapController.move(latLng, defaultZoom);
-        },
+          _mapReady = true;
+        }
       ),
       children: [
         TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: 'com.example.app',
         ),
+        MarkerLayer(markers: [_marker!]),
         RichAttributionWidget(
           attributions: [
             TextSourceAttribution(
