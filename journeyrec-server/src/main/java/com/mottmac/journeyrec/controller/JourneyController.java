1558a61b6c4bb5129037537184e0be785a9819bc
alistairrutherford <alistair.rutherford@gmail.com>
2023-11-22 21:40:05 +0000

    Added controller and more persistence.

--- journeyrec-server/src/main/java/com/mottmac/journeyrec/controller/JourneyController.java
+++ journeyrec-server/src/main/java/com/mottmac/journeyrec/controller/JourneyController.java
@@ -1,57 +1,68 @@
 package com.mottmac.journeyrec.controller;
 
+import com.mottmac.journeyrec.dao.JourneyPointRepository;
 import com.mottmac.journeyrec.dao.JourneyRepository;
 import com.mottmac.journeyrec.domain.Journey;
+import com.mottmac.journeyrec.domain.JourneyPoint;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class JourneyController {
-    private final JourneyRepository repository;
+    private final JourneyRepository journeyRepository;
+    private final JourneyPointRepository journeyPointRepository;
 
-    public JourneyController(JourneyRepository repository) {
-        this.repository = repository;
+    public JourneyController(JourneyRepository journeyRepository, JourneyPointRepository journeyPointRepository) {
+        this.journeyRepository = journeyRepository;
+        this.journeyPointRepository = journeyPointRepository;
     }
 
     @GetMapping("/Journeys")
-    Iterable<Journey> all() {
-        return repository.findAll();
+    public Iterable<Journey> all() {
+        return journeyRepository.findAll();
     }
     // end::get-aggregate-root[]
 
     @PostMapping("/Journeys")
-    Journey newJourney(@RequestBody Journey newJourney) {
-        return repository.save(newJourney);
+    public Journey addJourney(@RequestBody Journey newJourney) {
+        return journeyRepository.save(newJourney);
+    }
+
+    @PostMapping("/JourneyPoints")
+    public void  addJourneyPoints(@RequestBody List<JourneyPoint> points) {
+        for (JourneyPoint journeyPoint: points) {
+            journeyPointRepository.save(journeyPoint);
+        }
     }
 
     // Single item
 
     @GetMapping("/Journeys/{id}")
-    Journey one(@PathVariable Long id) throws JourneyNotFoundException {
+    public Journey one(@PathVariable Long id) throws JourneyNotFoundException {
 
-        return repository.findById(id)
+        return journeyRepository.findById(id)
                 .orElseThrow(() -> new JourneyNotFoundException(id));
     }
 
     @PutMapping("/Journeys/{id}")
-    Journey replaceJourney(@RequestBody Journey newJourney, @PathVariable Long id) {
+    public Journey replaceJourney(@RequestBody Journey newJourney, @PathVariable Long id) {
 
-        return repository.findById(id)
+        return journeyRepository.findById(id)
                 .map(Journey -> {
                     Journey.setJourneyType(newJourney.getJourneyType());
                     Journey.setDistance(newJourney.getDistance());
                     // TODO and the rest
-                    return repository.save(Journey);
+                    return journeyRepository.save(Journey);
                 })
                 .orElseGet(() -> {
                     newJourney.setId(id);
-                    return repository.save(newJourney);
+                    return journeyRepository.save(newJourney);
                 });
     }
 
     @DeleteMapping("/Journeys/{id}")
-    void deleteJourney(@PathVariable Long id) {
-        repository.deleteById(id);
+    public void deleteJourney(@PathVariable Long id) {
+        journeyRepository.deleteById(id);
     }
 }
