20052a7e7f8c14e4875644c17a03a1e38283ed41
alistairrutherford <alistair.rutherford@gmail.com>
2023-12-06 14:14:21 +0000

    Code tidy.

--- journeyrec-server/src/main/java/com/mottmac/journeyrec/controller/JourneyController.java
+++ journeyrec-server/src/main/java/com/mottmac/journeyrec/controller/JourneyController.java
@@ -3,8 +3,14 @@ package com.mottmac.journeyrec.controller;
 import com.mottmac.journeyrec.domain.JourneyComposite;
 import com.mottmac.journeyrec.service.JourneyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Journey Controller handles journey.
+ */
 @RestController
 public class JourneyController {
     private final JourneyService journeyService;
