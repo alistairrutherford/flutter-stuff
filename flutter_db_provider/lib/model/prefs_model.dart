47357f6ac9cb63389551f90281e759dd03e78ee0
alistairrutherford <rut19830@ISO-GB002201AB.localdomain>
2023-12-08 10:44:45 +0000

    Fixed device ID identifier for prefs.

--- flutter_db_provider/lib/model/prefs_model.dart
+++ flutter_db_provider/lib/model/prefs_model.dart
@@ -6,7 +6,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 /// which will be used when posting journeys to uniquely identify source
 /// of data.
 class SharedPreferencesModel extends ChangeNotifier {
-  static const String deviceID = "http://10.0.2.2:8080/journeyrec/journey";
+  static const String prefDeviceID = "DEVICE_ID";
 
   String? deviceId;
 
@@ -18,13 +18,13 @@ class SharedPreferencesModel extends ChangeNotifier {
     // Load and obtain the shared preferences for this app.
     final prefs = await SharedPreferences.getInstance();
 
-    deviceId = prefs.getString(deviceID);
+    deviceId = prefs.getString(prefDeviceID);
 
     if (deviceId == null) {
       // Generate
       var uuid = const Uuid();
       String id = uuid.v4();
-      await prefs.setString(deviceID, id);
+      await prefs.setString(prefDeviceID, id);
       deviceId = id;
     }
   }
