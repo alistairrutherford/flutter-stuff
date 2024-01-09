c9d1234e8d175377dfb85937fbee13add321d649
alistairrutherford <alistair.rutherford@gmail.com>
2024-01-09 22:36:54 +0000

    Updated gradle version.

--- rail/get-me-home/lib/arrivalsdepartures.dart
+++ rail/get-me-home/lib/arrivalsdepartures.dart
@@ -5,11 +5,19 @@ class ArrivalsDeparturesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Column(
-        children: [
-          Card(child: Text("Arrivals"),),
-          Card(child: Text("Departures"),)
-        ],
+    return Container(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+            child: Card(
+          child: Text("Arrivals"),
+        )),
+        Expanded(
+            child: Card(
+          child: Text("Departures"),
+        )),
+      ],
     ));
   }
 }
