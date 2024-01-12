0ff8eed1a8a9c00a75650500cd02ca0aa0b77f94
alistairrutherford <alistair.rutherford@gmail.com>
2024-01-12 17:57:44 +0000

    Added StatefulWrapper to handle fetching data for the first time.

--- rail/get-me-home/lib/arrivalsdepartures.dart
+++ rail/get-me-home/lib/arrivalsdepartures.dart
@@ -2,135 +2,166 @@ import 'package:flutter/material.dart';
 import 'package:get_me_home/model/timetable_model.dart';
 import 'package:provider/provider.dart';
 
+import 'model/network_model.dart';
+
 class ArrivalsDeparturesView extends StatelessWidget {
   const ArrivalsDeparturesView({super.key});
 
   @override
   Widget build(BuildContext context) {
     var model = context.watch<TimeTableModel>();
+
     var arrivals = model.arrivals;
     var departures = model.departusers;
 
-    // How do we update this
-    // var networkModel = context.watch<NetworkModel>();
-    // model.refresh(networkModel);
-
-    return Material(
-      child: Container(
-        color: Colors.indigo,
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Card(
-              child: ListTile(
-                contentPadding: EdgeInsets.only(
-                    left: 10.0, right: 10.0, top: 10, bottom: 10),
-                leading: Icon(
-                  Icons.train,
-                  color: Colors.white,
-                  size: 60,
-                ),
-                title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    // mainAxisAlignment: MainAxisAlignment.start,
-                    // mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text(
-                        "Arrival",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontSize: 30),
-                      ),
-                      SizedBox(height: 10),
-                      Row(mainAxisSize: MainAxisSize.max, children: [
-                        Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            // mainAxisAlignment: MainAxisAlignment.start,
-                            // mainAxisSize: MainAxisSize.max,
-                            children: <Widget>[
-                              Text('Origin',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black,
-                                      fontSize: 20)),
-                              SizedBox(height: 5),
-                              Text("Milngavie",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                      fontSize: 20))
-                            ]),
-                        SizedBox(width: 40),
-                        Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            // mainAxisAlignment: MainAxisAlignment.start,
-                            // mainAxisSize: MainAxisSize.max,
-                            children: <Widget>[
-                              Text('Destination',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black,
-                                      fontSize: 20)),
-                              SizedBox(height: 5),
-                              Text("Motherwell",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                      fontSize: 20))
-                            ]),
+    return StatefulWrapper(
+      onInit: () {
+        var networkModel = context.watch<NetworkModel>();
+        model.refresh(networkModel);
+      },
+      child: Material(
+        child: Container(
+          color: Colors.indigo,
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                child: ListTile(
+                  contentPadding: EdgeInsets.only(
+                      left: 10.0, right: 10.0, top: 10, bottom: 10),
+                  leading: Icon(
+                    Icons.train,
+                    color: Colors.white,
+                    size: 60,
+                  ),
+                  title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      // mainAxisAlignment: MainAxisAlignment.start,
+                      // mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          "Arrival",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 30),
+                        ),
+                        SizedBox(height: 10),
+                        Row(mainAxisSize: MainAxisSize.max, children: [
+                          Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              // mainAxisAlignment: MainAxisAlignment.start,
+                              // mainAxisSize: MainAxisSize.max,
+                              children: <Widget>[
+                                Text('Origin',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black,
+                                        fontSize: 20)),
+                                SizedBox(height: 5),
+                                Text("Milngavie",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                        fontSize: 20))
+                              ]),
+                          SizedBox(width: 40),
+                          Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              // mainAxisAlignment: MainAxisAlignment.start,
+                              // mainAxisSize: MainAxisSize.max,
+                              children: <Widget>[
+                                Text('Destination',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black,
+                                        fontSize: 20)),
+                                SizedBox(height: 5),
+                                Text("Motherwell",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                        fontSize: 20))
+                              ]),
+                        ]),
+                        Divider(
+                          height: 10,
+                        ),
+                        Row(mainAxisSize: MainAxisSize.max, children: [
+                          Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              // mainAxisAlignment: MainAxisAlignment.start,
+                              // mainAxisSize: MainAxisSize.max,
+                              children: <Widget>[
+                                Text('Arrival',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black,
+                                        fontSize: 20)),
+                                SizedBox(height: 5),
+                                Text("12:00:00",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                        fontSize: 20))
+                              ]),
+                          SizedBox(width: 40),
+                          Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              // mainAxisAlignment: MainAxisAlignment.start,
+                              // mainAxisSize: MainAxisSize.max,
+                              children: <Widget>[
+                                Text('ETA',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black,
+                                        fontSize: 20)),
+                                SizedBox(height: 5),
+                                Text("12:00:02",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                        fontSize: 20))
+                              ]),
+                        ]),
                       ]),
-                      Divider(
-                        height: 10,
-                      ),
-                      Row(mainAxisSize: MainAxisSize.max, children: [
-                        Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            // mainAxisAlignment: MainAxisAlignment.start,
-                            // mainAxisSize: MainAxisSize.max,
-                            children: <Widget>[
-                              Text('Arrival',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black,
-                                      fontSize: 20)),
-                              SizedBox(height: 5),
-                              Text("12:00:00",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                      fontSize: 20))
-                            ]),
-                        SizedBox(width: 40),
-                        Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            // mainAxisAlignment: MainAxisAlignment.start,
-                            // mainAxisSize: MainAxisSize.max,
-                            children: <Widget>[
-                              Text('ETA',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black,
-                                      fontSize: 20)),
-                              SizedBox(height: 5),
-                              Text("12:00:02",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                      fontSize: 20))
-                            ]),
-                      ]),
-                    ]),
-                tileColor: Colors.blue,
-                trailing: Icon(
-                  Icons.upload_file,
-                  color: Colors.white,
+                  tileColor: Colors.blue,
+                  trailing: Icon(
+                    Icons.upload_file,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+/// Helper class which adds an one time initialsation to StatelessWId
+class StatefulWrapper extends StatefulWidget {
+  final Function onInit;
+  final Widget child;
+
+  const StatefulWrapper({required this.onInit, required this.child});
+
+  @override
+  _StatefulWrapperState createState() => _StatefulWrapperState();
+}
+
+class _StatefulWrapperState extends State<StatefulWrapper> {
+  @override
+  void initState() {
+    if (widget.onInit != null) {
+      widget.onInit();
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
   }
 }
