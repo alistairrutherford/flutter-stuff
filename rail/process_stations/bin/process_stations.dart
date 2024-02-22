4b0a889aeb732d0d65e007867a2b88631cca481f
alistairrutherford <alistair.rutherford@gmail.com>
2024-02-22 17:55:02 +0000

    First stab at reading file.

--- rail/process_stations/bin/process_stations.dart
+++ rail/process_stations/bin/process_stations.dart
@@ -1,5 +1,23 @@
-import 'package:process_stations/station_service.dart' as process_stations;
+import 'dart:developer';
+import 'dart:io';
+import 'package:process_stations/station_service.dart';
+
+const String INPUT_DATA = "stations.json";
+
+String buildFilePath(String fileName) {
+  Directory current = Directory.current;
+
+  String path = current.toString().split(' ')[1].replaceAll('\'','') + "/data/" + fileName;
+
+  return path;
+}
 
 void main(List<String> arguments) {
+ StationService service = StationService();
+
+ String filePath = buildFilePath(INPUT_DATA);
+
+ log(filePath);
+
   // TODO read stations data, filter and write back.
 }
--- rail/process_stations/lib/station_service.dart
+++ rail/process_stations/lib/station_service.dart
@@ -1,13 +1,21 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:process_stations/stations.dart';
+
 
 class StationService {
 
   Future<List<Map>> readStations(String filePath) async {
+    ReferenceData referenceData = ReferenceData();
+
     var input = await File(filePath).readAsString();
+
     var map = jsonDecode(input);
-    return map['stations'];
+
+    referenceData.fromJson(map);
+
+    return map['Station'];
   }
 
   List<Map> filterStations(List<Map> data) {
