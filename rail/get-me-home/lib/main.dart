e6a13c2e28452382ed1840c703608dc3468ebe92
alistairrutherford <alistair.rutherford@gmail.com>
2024-05-29 23:25:37 +0100

    Build list of stations.

--- rail/get-me-home/lib/main.dart
+++ rail/get-me-home/lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:core';
+import 'dart:core';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_me_home/model/stations_model.dart';
 import 'package:get_me_home/model/timetable_model.dart';
 import 'package:provider/provider.dart';
 import 'arrivals_departures.dart';
+import 'dao/stations.dart';
 import 'model/network_model.dart';
 import 'model/prefs_model.dart';
 
@@ -47,15 +51,7 @@ class MainView extends StatefulWidget {
   State<MainView> createState() => _MainViewState();
 }
 
-enum IconLabel {
-  smile('Smile', Icons.sentiment_satisfied_outlined),
-  cloud(
-    'Cloud',
-    Icons.cloud_outlined,
-  ),
-  brush('Brush', Icons.brush_outlined),
-  heart('Heart', Icons.favorite);
-
+class IconLabel {
   const IconLabel(this.label, this.icon);
   final String label;
   final IconData icon;
@@ -66,9 +62,35 @@ class _MainViewState extends State<MainView> {
   final TextEditingController iconController = TextEditingController();
   IconLabel? selectedIcon;
 
+  /// Build dropdown list.
+  List<DropdownMenuEntry<IconLabel>> buildStations(StationsModel model) {
+    List<Station?>? stations = List<Station>.empty();
+    List<DropdownMenuEntry<IconLabel>> dropdownStations = List<DropdownMenuEntry<IconLabel>>.empty();
+
+     if (model.stations != null) {
+       if (model.stations!.stationsReferenceData != null) {
+         if (model.stations!.stationsReferenceData!.station != null) {
+           stations = model.stations!.stationsReferenceData!.station;
+
+           for (final station in stations!) {
+             String name = station!.name!;
+             IconLabel iconLabel = IconLabel(name, Icons.favorite);
+
+             DropdownMenuEntry<IconLabel> entry = new DropdownMenuEntry<IconLabel>(value: iconLabel, label: name);
+
+             dropdownStations.add(entry);
+           }
+         }
+        }
+     }
+
+     return dropdownStations;
+  }
+
   @override
   Widget build(BuildContext context) {
     var stationsModel = context.watch<StationsModel>();
+    List<DropdownMenuEntry<IconLabel>> dropdownMenuEntries = buildStations(stationsModel);
 
     return MaterialApp(
       title: 'Get Me Home',
@@ -106,16 +128,7 @@ class _MainViewState extends State<MainView> {
                     selectedIcon = icon;
                   });
                 },
-                dropdownMenuEntries:
-                IconLabel.values.map<DropdownMenuEntry<IconLabel>>(
-                      (IconLabel icon) {
-                    return DropdownMenuEntry<IconLabel>(
-                      value: icon,
-                      label: icon.label,
-                      leadingIcon: Icon(icon.icon),
-                    );
-                  },
-                ).toList(),
+                dropdownMenuEntries: dropdownMenuEntries.toList(),
               ),
             ),
             const Expanded(
